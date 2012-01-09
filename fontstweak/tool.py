7cd0f6f3018610b0a0f8da7189df3b5adb3834ec
James Ni <kent.neo@gmail.com>
2012-01-09 14:16:10 +0800

    Add Language chooser and button

--- fontstweak/tool.py
+++ fontstweak/tool.py
@@ -22,6 +22,7 @@
 
 import sys
 from gi.repository import Gtk
+from gi.repository import GObject
 	
 __all__ = (
 	    "FontsTweakTool",
@@ -30,10 +31,59 @@ __all__ = (
 class FontsTweakTool:
     def destroy(self, args):
         Gtk.main_quit()
-    
+   
+    def selectClicked(self, *args):
+	#Get the lang from the list of languages
+        rc = self.langView.get_selection().get_selected()
+        if rc:
+            model, iter = rc
+            defaultLang =  self.langStore.get_value(iter, 0)
+            sysfontacm = self.langStore.get_value(iter, 1)
+            sysfont = self.langStore.get_value(iter, 2)
+            fullName = self.langStore.get_value(iter, 3)
+	
+     	list_iter = self.lang_list.append()
+        self.lang_list.set(list_iter, 0, "en_US.UTF-8")
+        self.lang_list.set(list_iter, 1, "iso01")
+        #self.lang_list.set_value(iter, 2, 'lat0-sun16')
+        #self.lang_list.set_value(iter, 3, 'English (USA)')
+	
+        column = Gtk.TreeViewColumn(None, Gtk.CellRendererText(), text=3)
+	self.lang_view.append_column(column)
+ 
     def closeClicked(self, *args):
-	Gtk.main_quit()	
-     
+	Gtk.main_quit()
+
+    def addlangClicked(self, *args):
+        lang_dialog = Gtk.Dialog() 	
+        self.toplevel = Gtk.VBox()
+        self.langStore = Gtk.ListStore(GObject.TYPE_STRING, GObject.TYPE_STRING,
+                                       GObject.TYPE_STRING, GObject.TYPE_STRING)
+        self.title = Gtk.Label("Language Selection")
+            
+        iter = self.langStore.append()
+        self.langStore.set_value(iter, 0, 'en_US.UTF-8')
+        self.langStore.set_value(iter, 1, 'iso01')
+        self.langStore.set_value(iter, 2, 'lat0-sun16')
+        self.langStore.set_value(iter, 3, 'English (USA)')
+
+        self.langView = Gtk.TreeView(self.langStore)
+        self.col = Gtk.TreeViewColumn(None, Gtk.CellRendererText(), text=3)
+        self.langView.append_column(self.col)
+        self.langView.set_property("headers-visible", False)
+	content = lang_dialog.get_content_area()
+	content.add(self.langView)
+	
+	self.bb = Gtk.HButtonBox()
+        self.bb.set_layout(Gtk.ButtonBoxStyle.END)
+        self.bb.set_spacing(12)
+
+        action = lang_dialog.get_action_area()
+        self.okButton = Gtk.Button('Select')
+        self.okButton.connect("clicked", self.selectClicked)
+        action.add(self.okButton)
+	lang_dialog.show_all()    
+ 
     def __init__(self):
         builder = Gtk.Builder()
         builder.add_from_file("fontstools.ui") 
@@ -42,9 +92,15 @@ class FontsTweakTool:
 	self.window.set_title("fonts-tweak-tool")
         self.window.set_size_request(640, 480)        
 	
+	self.lang_view = builder.get_object("treeview1")
+	self.lang_list = builder.get_object("lang_list")
+	
 	self.close_button = builder.get_object("button2")
 	self.close_button.connect("clicked", self.closeClicked)
-   
+  
+	self.addlang_button = builder.get_object("add-lang")
+        self.addlang_button.connect("clicked", self.addlangClicked)
+
 def main(argv):
     tool = FontsTweakTool()
     tool.window.show_all()
